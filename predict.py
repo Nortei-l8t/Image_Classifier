a7cd783c40f2d6a02f2733fcfaa29549d079b34a
Nortei-l8t <57709872+Nortei-l8t@users.noreply.github.com>
2019-12-13 16:27:10 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -16,10 +16,7 @@ import argparse
 
 import futils
 
-#Command Line Arguments
-
-ap = argparse.ArgumentParser(
-    description='predict-file')
+ap = argparse.ArgumentParser(description='predict-file')
 ap.add_argument('input_img', default='paind-project/flowers/test/1/image_06752.jpg', nargs='*', action="store", type = str)
 ap.add_argument('checkpoint', default='/home/workspace/paind-project/checkpoint.pth', nargs='*', action="store",type = str)
 ap.add_argument('--top_k', default=5, dest="top_k", action="store", type=int)
@@ -33,28 +30,15 @@ power = pa.gpu
 input_img = pa.input_img
 path = pa.checkpoint
 
-
-
-training_loader, testing_loader, validation_loader = futils.load_data()
-
-
-futils.load_checkpoint(path)
-
-
-with open('cat_to_name.json', 'r') as json_file:
-    cat_to_name = json.load(json_file)
-
-
-probabilities = futils.predict(path_image, model, number_of_outputs, power)
-
-
-labels = [cat_to_name[str(index + 1)] for index in np.array(probabilities[1][0])]
-probability = np.array(probabilities[0][0])
-
-
-i=0
-while i < number_of_outputs:
-    print("{} with a probability of {}".format(labels[i], probability[i]))
-    i += 1
-
+def main():
+    model=iutils.load_checkpoint(path)
+    with open('cat_to_name.json', 'r') as json_file:
+        cat_to_name = json.load(json_file)
+    probabilities = iutils.predict(path_image, model, number_of_outputs, device)
+    labels = [cat_to_name[str(index + 1)] for index in np.array(probabilities[1][0])]
+    probability = np.array(probabilities[0][0])
+    i=0
+    while i < number_of_outputs:
+        print("{} with a probability of {}".format(labels[i], probability[i]))
+        i += 1
 print("You are here.")
