886f6e2435a64aabc92fcc7f8e85b91176213b36
Nortei-l8t <57709872+Nortei-l8t@users.noreply.github.com>
2019-12-13 16:57:22 +0000

    Update futils.py

--- futils.py
+++ futils.py
@@ -15,12 +15,9 @@ from PIL import Image
 from collections import OrderedDict
 import argparse
 
-arch = {"vgg16":25088,
-        "densenet121":1024
-        }
+arch = {"vgg19":25088, "densenet121":1024}
 
 def transform_image(root):
-
     data_dir = root
     train_dir = data_dir + '/train'
     valid_dir = data_dir + '/valid'
@@ -42,17 +39,14 @@ def transform_image(root):
                                       transforms.ToTensor(),
                                       transforms.Normalize([0.485, 0.456, 0.406],
                                                            [0.229, 0.224, 0.225])])}
-    image_datasets = {'train_data' : datasets.ImageFolder(data_dir + '/train', transform=data_transforms['train_transforms']),
+    image_datasets = {
+            'train_data' : datasets.ImageFolder(data_dir + '/train', transform=data_transforms['train_transforms']),
     'test_data' : datasets.ImageFolder(data_dir + '/test', transform=data_transforms['test_transforms']),
     'valid_data' : datasets.ImageFolder(data_dir + '/valid', transform=data_transforms['validation_transforms'])
-                 }
-    
+                        }
     return image_datasets['train_data'] , image_datasets['valid_data'], image_datasets['test_data']
 
-
-
 def load_data(root):
-    
     data_dir = root    
     tr_data,val_data,te_data=transform_image(data_dir)
     
@@ -62,19 +56,16 @@ def load_data(root):
     
     return dataloaders['trainloader'] , dataloaders['validloader'], dataloaders['testloader']
 
-
 train_data,valid_data,test_data=transform_image('./flowers/')
 trdl,vdl,tsdl=load_data('./flowers/')
 
-def network_construct(structure='vgg16',dropout=0.5, hidden_layer1 = 4096,lr = 0.001,device='gpu'):
-
+def network_construct(structure='vgg19',dropout=0.5, hidden_layer1 = 4096,lr = 0.001,device='gpu'):
     if structure == 'vgg16':
         model = models.vgg16(pretrained=True)
     elif structure == 'densenet121':
         model = models.densenet121(pretrained=True)
     else:
-        print("Please try for vgg16 or densenet121 only")
-
+        print("Please try for vgg19 or densenet121 only")
 
     for param in model.parameters():
         param.requires_grad = False
@@ -87,8 +78,7 @@ def network_construct(structure='vgg16',dropout=0.5, hidden_layer1 = 4096,lr = 0
                           ('d_out2',nn.Dropout(dropout)),
                           ('fc3', nn.Linear(1024, 102)),
                           ('output', nn.LogSoftmax(dim=1))
-                          ]))
-
+                                                ]))
 
     model.classifier = classifier
     criterion = nn.NLLLoss()
@@ -98,7 +88,6 @@ def network_construct(structure='vgg16',dropout=0.5, hidden_layer1 = 4096,lr = 0
         model.cuda()
 
     return model, criterion, optimizer
-
 
 def do_deep_learning(model, criterion, optimizer, epochs = 3, print_every=40, loader=0, device='gpu'):
     steps = 0
@@ -112,7 +101,6 @@ def do_deep_learning(model, criterion, optimizer, epochs = 3, print_every=40, lo
 
             optimizer.zero_grad()
 
-            # Forward and backward passes
             outputs = model.forward(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
@@ -124,7 +112,6 @@ def do_deep_learning(model, criterion, optimizer, epochs = 3, print_every=40, lo
                 model.eval()
                 vlost = 0
                 accuracy=0
-
 
                 for ii, (inputs2,labels2) in enumerate(vdl):
                     optimizer.zero_grad()
@@ -142,19 +129,15 @@ def do_deep_learning(model, criterion, optimizer, epochs = 3, print_every=40, lo
                 vlost = vlost / len(vdl)
                 accuracy = accuracy /len(vdl)
 
-
-
                 print("Epoch: {}/{}... ".format(e+1, epochs),
                       "Loss: {:.4f}".format(running_loss/print_every),
                       "Validation Lost {:.4f}".format(vlost),
                        "Accuracy: {:.4f}".format(accuracy))
 
-
                 running_loss = 0
                 model.train()
 
-
-def save_checkpoint(model=0,path='checkpoint.pth',structure ='vgg16', hidden_layer1 = 4096,dropout=0.5,lr=0.001,epochs=3):
+def save_checkpoint(model=0,path='checkpoint.pth',structure ='vgg19', hidden_layer1 = 4096,dropout=0.5,lr=0.001,epochs=3):
 
     model.class_to_idx =  train_data.class_to_idx
     model.cpu
@@ -166,7 +149,6 @@ def save_checkpoint(model=0,path='checkpoint.pth',structure ='vgg16', hidden_lay
                 'state_dict':model.state_dict(),
                 'class_to_idx':model.class_to_idx},
                 path)
-
 
 def load_checkpoint(path='checkpoint.pth'):
     checkpoint = torch.load(path)
@@ -182,23 +164,20 @@ def load_checkpoint(path='checkpoint.pth'):
     
     return model
 
-
 def process_image(image_path='/home/workspace/aipnd-project/flowers/test/1/image_06752.jpg'):
 
 
     proc_img = Image.open(image_path)
 
-    prepoceess_img = transforms.Compose([
+    prepoceess_img = transforms.Compose ([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
+                                                        ])
 
     pymodel_img = prepoceess_img(proc_img)
     return pymodel_img
-
 
 def predict(image='/home/workspace/aipnd-project/flowers/test/1/image_06752.jpg', model=0, topk=5,device='gpu'):
 
@@ -215,7 +194,5 @@ def predict(image='/home/workspace/aipnd-project/flowers/test/1/image_06752.jpg'
     else:
         with torch.no_grad():
             output=model.forward(img_torch)
-
     probability = F.softmax(output.data,dim=1)
-
     return probability.topk(topk)
